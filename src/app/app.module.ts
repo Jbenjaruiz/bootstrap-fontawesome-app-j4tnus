8c09167711d6c42e418643d34758bbdc80fc4adf
Jbenjaruiz <git@stackblitz.com>
2025-06-06 01:13:57 -0600

    Register detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
+import { DetailsDialogComponent } from './details-dialog/details-dialog.component';
 import { ExcelService } from './services/excel.service';
 
 @NgModule({
@@ -33,6 +34,7 @@ import { ExcelService } from './services/excel.service';
     HeaderComponent,
     FooterComponent,
     ContentComponent,
+    DetailsDialogComponent,
   ],
   providers: [ExcelService, DatePipe],
   bootstrap: [AppComponent],
