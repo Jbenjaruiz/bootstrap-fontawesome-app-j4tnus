c22e8f629e0e7a44211451caa59fc68a42d54ec7
Jbenjaruiz <git@stackblitz.com>
2025-06-06 01:18:42 -0600

    New package modules imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { ClipboardModule } from '@angular/cdk/clipboard'; // Para copiar
+
 import { DatePipe } from '@angular/common';
 
 import { AppComponent } from './app.component';
@@ -28,6 +33,9 @@ import { ExcelService } from './services/excel.service';
     MatInputModule,
     MatFormFieldModule,
     MatButtonModule,
+    MatDialogModule, // Añade el módulo de Dialog
+    MatIconModule, // Para usar íconos como el de "copiar"
+    ClipboardModule, // Añade el módulo de Clipboard
   ],
   declarations: [
     AppComponent,
